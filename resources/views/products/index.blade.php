cf0d70485ad623d73025fa4001f1595d6e1303c0
Hyfos2 <bthighfore@gmail.com>
2017-12-30 14:36:13 +0200

    working form home

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -15,15 +15,15 @@
       {!! Form::open(['url' => 'createProduct', 'method' => 'post', 'class' => 'form-horizontal', 'id'=>"addProductsForm" ,'v-on:submit'=>"validateForm" ]) !!}
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-      <div class="form-group"  v-bind:class="{ 'has-error': submition && wrongName }">
+      <div class="form-group">
           {!! Form::label('Name', 'Name', array('class' => 'col-md-3 control-label')) !!}
           <div class="col-md-6" >
-              {!! Form::text('name',NULL,['class' => 'form-control input-sm','id' => 'name','v-model'=>'name']) !!}
+              {!! Form::text('name',NULL,['class' => 'form-control input-sm','id' => 'name','v-model'=>'name','autocomplete'=>'off']) !!}
               <span class="help-block"  v-cloak v-if="submition && wrongName">@{{nameFB}}</span>
           </div>
       </div>
 
-      <div class="form-group"  v-bind:class="{ 'has-error': submition && wrongTypeId }">
+      <div class="form-group">
           {!! Form::label('Product Type', 'Product Type', array('class' => 'col-md-3 control-label')) !!}
           <div class="col-md-6">
              <select @change="updateDroneType($event.target.value)"  name="productTypeId" v-cloak class="form-control" id="droneTypeData"  v-model="productTypeId">
@@ -37,27 +37,29 @@
           </div>
       </div>
 
-      <div class="form-group"  v-bind:class="{ 'has-error': submition && wrongSpecification }">
+      <div class="form-group">
           {!! Form::label('Specification', 'Specification', array('class' => 'col-md-3 control-label')) !!}
           <div class="col-md-6">
-              {!! Form::textarea('specification',NULL,['class' => 'form-control input-sm','id' => 'specification' , 'placeholder'=>'e.g describe the product here...' ,'v-model'=>'specification']) !!}
+              {!! Form::textarea('specification',NULL,['class' => 'form-control input-sm','id' => 'specification' , 'placeholder'=>'e.g describe the product here...' ,'v-model'=>'specification','autocomplete'=>'off']) !!}
               <span class="help-block"  v-cloak v-if="submition && wrongSpecification">@{{specificationFB }}</span>
 
           </div>
       </div>
 
-      <div class="form-group"  v-bind:class="{ 'has-error': submition && wrongPrice }">
+      <div class="form-group">
           {!! Form::label('Price', 'Price', array('class' => 'col-md-3 control-label')) !!}
           <div class="col-md-6">
-              {!! Form::text('price',NULL,['class' => 'form-control input-sm','id' => 'price','v-model'=>'price']) !!}
+               <input name="price" class="form-control" v-model="price" v-validate="'decimal'" :class="{'input': true, 'is-danger': errors.has('price') }" type="text" autocomplete= "off" placeholder="100.00">
+            <span style="color: red;"  v-cloak v-show="errors.has('price')" class="help is-danger">@{{ errors.first('price') }}</span>
               <span class="help-block" v-cloak v-if="submition && wrongPrice">@{{priceFB }}</span>
           </div>
       </div>
 
-      <div class="form-group"  v-bind:class="{ 'has-error': submition && wrongUnit }">
+      <div class="form-group">
           {!! Form::label('Quantity', 'Quantity', array('class' => 'col-md-3 control-label')) !!}
           <div class="col-md-6">
-              {!! Form::text('initialQty',NULL,['class' => 'form-control input-sm','id' => 'initialQty','v-model'=>'initialQty']) !!}
+            <input name="initialQty" class="form-control" v-model="initialQty" v-validate="'numeric'" :class="{'input': true, 'is-danger': errors.has('initialQty') }" type="text" autocomplete= "off">
+            <span style="color: red;"  v-cloak v-show="errors.has('initialQty')" class="help is-danger">@{{ errors.first('initialQty') }}</span>
               <span class="help-block" v-cloak v-if="submition && wrongUnit">@{{unitFB }}</span>
           </div>
       </div>
--- resources/views/settings/preferences.blade.php
+++ resources/views/settings/preferences.blade.php
@@ -1,7 +1,7 @@
 {!! Form::model($userDetails,['url' => 'preference', 'method' => 'post', 'class' => 'form-horizontal','v-on:submit'=>"preferenceForm" ,'id'=>'userPreferenceForm' ]) !!}
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
   
-      <div class="form-group"  v-bind:class="{ 'has-error': submition && wrongPassword }">
+      <div class="form-group">
 
           {!! Form::label('New Password', 'New Password', array('class' => 'col-xs-3 control-label')) !!}
           <div class="col-xs-8" >
@@ -12,7 +12,7 @@
       </div>
 
 
-      <div class="form-group"  v-bind:class="{ 'has-error': submition && wrongPwdVerification }">
+      <div class="form-group">
           {!! Form::label('Confirm Password', 'Confirm Password', array('class' => 'col-xs-3 control-label')) !!}
           <div class="col-xs-8">
              <input type="password" name="confirm_password"  class="form-control input-sm" id="confirm_password" v-model='confirm_password'>
