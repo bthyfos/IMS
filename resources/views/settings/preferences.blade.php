b0b3929fc57e0a06bdd9055556958a92fbf5a14d
Hyfos2 <bthighfore@gmail.com>
2017-12-30 15:14:51 +0200

    30 of December

--- resources/views/settings/preferences.blade.php
+++ resources/views/settings/preferences.blade.php
@@ -5,28 +5,28 @@
 
           {!! Form::label('New Password', 'New Password', array('class' => 'col-xs-3 control-label')) !!}
           <div class="col-xs-8" >
-             <input type="password" name="password"  class="form-control input-sm" id="password"  v-model='password'>
-              <!-- {!! Form::password('password',NULL,['class' => 'form-control input-sm','id' => 'name','v-model'=>'name']) !!} -->
+
+
+            <input name="password" v-validate="{ is: confirm_password }"  confirmed:{confirm_password} class="form-control"  :class="{'input': true, 'is-danger': errors.has('password') }" type="password"  v-model='password'>
+            <span  v-cloak v-show="errors.has('password')" class="help-block">@{{ errors.first('password') }}</span>
               <span class="help-block"  v-cloak v-if="submition && wrongPassword">@{{passwordFB}}</span>
           </div>
+
+
       </div>
+
 
 
       <div class="form-group">
           {!! Form::label('Confirm Password', 'Confirm Password', array('class' => 'col-xs-3 control-label')) !!}
           <div class="col-xs-8">
-             <input type="password" name="confirm_password"  class="form-control input-sm" id="confirm_password" v-model='confirm_password'>
-              <!-- {!! Form::text('specification',NULL,['class' => 'form-control input-sm','id' => 'specification' , 'placeholder'=>'e.g describe the product here...' ,'v-model'=>'specification']) !!} -->
+             <input name="confirm_password" class="form-control"  :class="{'input': true, 'is-danger': errors.has('confirm_password') }" type="password" v-model='confirm_password'>
+               <span  v-cloak v-show="errors.has('confirm_password')" class="help-block">@{{ errors.first('confirm_password') }}</span>
+
               <span class="help-block"  v-cloak v-if="submition && wrongPwdVerification">@{{passwordVerificationFB}}</span>
 
           </div>
       </div>
-
-       <div class="form-group" v-bind:class="{ 'has-error': submition && passwordMisMatch }">
-                            <div class="col-md-3"></div>
-                             <div class="col-md-6"><span class="help-block"  v-cloak v-if="submition && passwordMisMatch">@{{passwordMisMatchFB}}</span>
-                             </div>
-                             </div>
 
       <div class="form-group" >
           <div class="col-md-offset-3 col-md-10">
